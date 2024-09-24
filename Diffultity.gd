6a934041f4ac928abf76f7487c3c4ebebe496293
OliverTheLittleLion <bjohnson5@ftstudent.org>
2024-09-24 13:15:25 -0400

    started Diffulity

---
+++ Diffultity.gd
@@ -0,0 +1,17 @@
+extends OptionButton
+func _on_option_button_item_selected(index:0):
+	pass # Replace with function body.
+
+
+func _on_option_button_item_selected(index:1):
+	pass # Replace with function body.
+
+func _on_option_button_item_selected(index:2):
+	pass # Replace with function body.
+
+func _on_option_button_item_selected(index:3):
+	pass # Replace with function body.
+
+func _on_option_button_item_selected(index:4):
+	pass # Replace with function body.
+
--- start.gd
+++ start.gd
@@ -5,3 +5,5 @@ func _on_button_down():
 	print("i pressed start")
 	get_tree().change_scene_to_file("res://game.tscn")
 	pass # Replace with function body.
+
+
